bf29931f588599c2381cf10afd98e402b8abedc5
aakash <aakashmathai@gmail.com>
2017-05-21 23:28:48 +0530

    2-models and html (3rd modal not done) done(front end almost done)

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from invoice.models import invoice
+from invoice.models import bills
 from invoice.models import customer
 from invoice.models import products
-admin.site.register(invoice)
+admin.site.register(bills)
 admin.site.register(customer)
 admin.site.register(products)
--- invoice/views.py
+++ invoice/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.utils import timezone
+from django.http import HttpResponse
+from django.template import loader
+
 from .models import customer
 from .models import products
-from .models import invoice
+from .models import bills
 
 def invoice(request):
-    	return render(request, 'invoice/billing.html', {})
+    customer_table = customer.objects.all()
+    products_table = products.objects.all()
+    bills_table = bills.objects.all()
+    new = next_bill()+1
+    context = {
+        'customer':customer_table,
+        'products':products_table,
+        'bills':bills_table,
+        'new':new,
+    }
+    return render(request, 'invoice/billing.html', context)
 
+def next_bill():
+    last_record = bills.objects.all().last()
+    if not last_record:
+        return 0
+    else:
+        return last_record.id
